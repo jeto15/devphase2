eb8ac54e84645cffe19c4a9d5b5f805ddbdeb0b4
Jet <jetpersonal7@gmail.com>
2024-06-17 22:27:04 +0800

    update db

--- admin/.config/db.php
+++ admin/.config/db.php
@@ -1,6 +1,5 @@
 <?php 
 date_default_timezone_set('Asia/Manila'); 
 require_once "mysql.php";
-$db = new MySQL("localhost", "u285821606_develop", "bTG*evKybGh9X6si", "u285821606_candayahdc_dev", true);
- 
- 
+$db = new MySQL("localhost", "u285821606_develop", "bTG*evKybGh9X6si", "u285821606_candayahdc_dev", true); 
+ //123213
