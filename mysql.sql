e209678e3dddfd880d502daefdc0215049aa2f07
Jet <jetpersonal7@gmail.com>
2024-07-16 07:11:10 +0800

    asdf

--- mysql.sql
+++ mysql.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 15, 2024 at 03:20 PM
+-- Generation Time: Jul 16, 2024 at 01:10 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -36,26 +36,6 @@ CREATE TABLE `laboratory` (
   `lastmodifieddate` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `laboratory`
---
-
-INSERT INTO `laboratory` (`Id`, `Name`, `Description`, `created_date`, `listprice`, `lastmodifieddate`) VALUES
-(1, 'CBC', NULL, '2024-05-01 20:51:29', 5, NULL),
-(2, 'UA', 'This for testing ', '2024-05-01 20:51:29', 5, '2024-07-11 13:51:19'),
-(3, 'PT UA', NULL, '2024-05-01 20:51:29', 5, NULL),
-(4, 'STOOL EXAM', 'asdfasdfas', '2024-05-01 20:51:29', 6, '2024-07-05 16:13:59'),
-(5, 'SGPT', NULL, '2024-05-01 20:51:29', 52, NULL),
-(6, 'SGOT', 'asdfasdf', '2024-05-01 20:51:29', 122, '2024-07-05 16:10:44'),
-(7, 'LIPID PANEL 422', '', '2024-05-01 20:51:29', 23, '2024-07-05 16:14:27'),
-(8, 'BUA', NULL, '2024-05-01 20:51:29', 25, NULL),
-(9, 'BUN', NULL, '2024-05-01 20:51:29', 10, NULL),
-(10, 'CREATININE', NULL, '2024-05-01 20:51:29', 5, NULL),
-(21, 'Lab', 'test dfasdf', '2024-07-05 15:21:00', 1000, '2024-07-05 16:10:29'),
-(22, 'LIPID PANEL123', 'test', '2024-07-05 15:25:09', 23, '2024-07-05 16:14:23'),
-(23, 'LIPID PANEL', 'test123', '2024-07-05 15:25:35', 23, '2024-07-05 15:25:35'),
-(24, 'TEST LAB', 'For Testing Only', '2024-07-11 13:49:45', 1000, '2024-07-11 13:49:45');
-
 -- --------------------------------------------------------
 
 --
@@ -75,16 +55,6 @@ CREATE TABLE `medicine` (
   `ceated_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `lastmodified_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `medicine`
---
-
-INSERT INTO `medicine` (`Id`, `Name`, `Brand`, `DosageForm`, `Strength`, `Manufacturer`, `ExpiryDate`, `Price`, `Description`, `ceated_date`, `lastmodified_date`) VALUES
-(3, 'Name', 'Brank', 'Tablet', '500mg', 'Manacutrere', '2024-07-15', 200.00, 'test ', '2024-07-04 06:12:42', '2024-07-04 06:12:42'),
-(4, 'Name1', 'Brank1', 'Tablet', '300mg', 'Manacutrere', '2024-07-15', 200.00, 'test ', '2024-07-04 06:12:49', '2024-07-04 06:55:17'),
-(5, 'Name3', 'Brank3', 'Tablet', '500mg', 'Manacutrere', '2024-07-15', 100.00, 'test ', '2024-07-04 06:12:53', '2024-07-11 06:10:20'),
-(6, 'Name 4', 'Brank 4', 'Tablet', '50 ml', 'Test Record Manufacturer', NULL, 5.00, 'Test Record Manufacturer Tablet', '2024-07-04 07:10:40', '2024-07-04 07:10:40');
 
 -- --------------------------------------------------------
 
@@ -108,22 +78,6 @@ CREATE TABLE `patients` (
   `modifiedby` int(11) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `patients`
---
-
-INSERT INTO `patients` (`first_name`, `last_name`, `middle_name`, `age`, `gender`, `birthday`, `contact_number`, `patient_number`, `address`, `Created Date`, `Id`, `createdby`, `modifiedby`) VALUES
-('Jason', 'Test', 'Person', '40', 'Male', '1973-02-15', '09123231231', 'P-1', 'Purok 5, Brgy San Roques Ozamiz City ', '2024-05-17 05:38:00', 5, NULL, 1),
-('Mela', 'Test', 'Person', '25', 'Female', '1959-06-17', '123213123123', 'P-2', 'aaaa asaaaaaaa', '2024-05-17 05:38:45', 6, NULL, NULL),
-('Asdfasdf', 'Sadf', 'Asdfsadf', '22', 'Male', '2023-02-01', 'Asdfasdf', 'P-3', 'sasdf', '2024-05-17 05:40:40', 7, NULL, NULL),
-('Aa', 'Asdf', 'Asdf', '', 'Male', '1994-02-03', '', 'P-test', '', '2024-05-17 05:42:30', 8, NULL, NULL),
-('Aa', 'Asdf', 'Asdf', '54', 'Female', '1988-02-05', '312312312', 'Tewtw ', 'adfasdfasdf', '2024-05-17 05:43:10', 9, NULL, NULL),
-('Sddd', 'Dddd', 'Dddasdfa', '54', 'Male', '1988-02-05', '02087599036', 'Asdfasdf', 'Test Addresss', '2024-05-17 05:43:38', 10, NULL, NULL),
-('Asfa', 'Asdf', 'Asdfasdf', '34', 'Female', '2007-03-17', '911111111111', 'P-24', 'Test address', '2024-05-25 07:16:44', 11, NULL, NULL),
-('Test', 'Tttt', 'Ttt', '46', 'Male', '2008-02-18', '123213123', 'P123', 'test', '2024-06-01 21:37:29', 12, 1, 1),
-('Test', 'Tttt', 'Ttt', '46', 'Male', '2008-02-18', '123213123', 'P-test', 'asdf', '2024-06-04 21:37:27', 13, 1, 0),
-('', '', '', '', 'Male', '', '', 'P-3', '', '2024-06-04 22:03:28', 14, 1, 0);
-
 -- --------------------------------------------------------
 
 --
@@ -140,17 +94,62 @@ CREATE TABLE `patient_file_library` (
   `patient_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `patient_file_library`
+-- Table structure for table `patient_request_table`
 --
 
-INSERT INTO `patient_file_library` (`id`, `description`, `isavatar`, `type`, `name`, `created_date`, `patient_id`) VALUES
-(5, 'Some description', 'Yes', 'image/jpeg', '6655e162e3ace.jpg', '2024-05-28 21:51:30', 6),
-(6, 'Some description', 'Yes', 'image/jpeg', '6655e3cd203e7.jpg', '2024-05-28 22:01:49', 5),
-(7, 'Some description', 'Yes', 'image/jpeg', '6655e4e7594fd.jpg', '2024-05-28 22:06:31', 7),
-(8, 'Some description', 'Yes', 'image/jpeg', '66587fa0f2b74.jpg', '2024-05-30 21:31:13', 5),
-(9, 'Some description', 'Yes', 'image/jpeg', '66588094d2c1b.jpg', '2024-05-30 21:35:16', 5),
-(10, 'Some description', 'Yes', 'image/jpeg', '665880ddc12a2.jpg', '2024-05-30 21:36:29', 5);
+CREATE TABLE `patient_request_table` (
+  `Id` int(11) NOT NULL,
+  `Description` text DEFAULT NULL,
+  `patient_Id` int(11) NOT NULL,
+  `Status` varchar(255) NOT NULL DEFAULT 'Draft',
+  `created_date` datetime DEFAULT NULL,
+  `created_by_Id` int(11) DEFAULT NULL,
+  `modified_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `modified_by` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prq_description_table`
+--
+
+CREATE TABLE `prq_description_table` (
+  `Id` int(11) NOT NULL,
+  `patient_request_Id` int(11) NOT NULL,
+  `patient_Id` text NOT NULL,
+  `modified_by_Id` int(11) DEFAULT NULL,
+  `modified_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `Description` text NOT NULL,
+  `Type` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prq_laboratory_table`
+--
+
+CREATE TABLE `prq_laboratory_table` (
+  `Id` int(11) NOT NULL,
+  `Status` text NOT NULL DEFAULT 'Unpaid',
+  `medicine_Id` int(11) DEFAULT NULL,
+  `laboratory_Id` int(11) DEFAULT NULL,
+  `isOther` tinyint(1) DEFAULT NULL,
+  `is_other_request` varchar(255) DEFAULT NULL,
+  `OtherType` varchar(255) DEFAULT NULL,
+  `Description` text DEFAULT NULL,
+  `patient_request_Id` int(11) NOT NULL,
+  `Qty` int(11) DEFAULT NULL,
+  `UnitPrice` double DEFAULT NULL,
+  `patient_Id` int(11) NOT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `created_by_Id` int(11) DEFAULT NULL,
+  `assignedDocotor` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -234,6 +233,24 @@ ALTER TABLE `patient_file_library`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `patient_request_table`
+--
+ALTER TABLE `patient_request_table`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `prq_description_table`
+--
+ALTER TABLE `prq_description_table`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `prq_laboratory_table`
+--
+ALTER TABLE `prq_laboratory_table`
+  ADD PRIMARY KEY (`Id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -255,25 +272,43 @@ ALTER TABLE `user_permession`
 -- AUTO_INCREMENT for table `laboratory`
 --
 ALTER TABLE `laboratory`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `medicine`
 --
 ALTER TABLE `medicine`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `patients`
 --
 ALTER TABLE `patients`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `patient_file_library`
 --
 ALTER TABLE `patient_file_library`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `patient_request_table`
+--
+ALTER TABLE `patient_request_table`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `prq_description_table`
+--
+ALTER TABLE `prq_description_table`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `prq_laboratory_table`
+--
+ALTER TABLE `prq_laboratory_table`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
